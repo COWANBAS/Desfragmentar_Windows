26c20e1863e00c824cd62d17eba1d657c7a028ca
COWANBAS <131304556+COWANBAS@users.noreply.github.com>
2024-12-27 17:50:23 -0300

    Add files via upload

---
+++ Defragment.bat
@@ -0,0 +1,14 @@
+@Echo File by Kauan Fonseca
+C:
+cls
+:: Desfragmentar ::
+@Echo Off
+echo.
+echo -----------------------------------
+echo          Desfragmentando
+echo -----------------------------------
+echo.
+
+Defrag C:
+Defrag D:
+pause
